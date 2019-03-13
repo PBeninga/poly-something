36dedec880cbbb9b46fea8b5f705622c943c9c5d
Mackinnon Buck <mackinnon.buck@gmail.com>
2019-03-13 12:01:35 -0700

    Added correct file names

--- main.js
+++ main.js
@@ -59,10 +59,10 @@ app.use(function(req, res, next) {
 app.use(CnnPool.router);
 
 // Load all subroutes
-app.use('/REST/Msgs', require('./Routes/Conversation/Msgs.js'))
+app.use('/REST/Cmts', require('./Routes/Conversation/Cmts.js'))
 app.use('/REST/Prss', require('./Routes/Account/Prss.js'));
 app.use('/REST/Ssns', require('./Routes/Account/Ssns.js'));
-app.use('/REST/Cnvs', require('./Routes/Conversation/Cnvs.js'));
+app.use('/REST/Prjs', require('./Routes/Conversation/Prjs.js'));
 
 // Special debugging route for /DB DELETE.  Clears all table contents,
 //resets all auto_increment keys to start at 1, and reinserts one admin user.
